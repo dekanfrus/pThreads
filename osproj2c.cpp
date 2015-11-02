4e1e236c7c8f5133c8a8ce613b6e58ecdece54da
dekanfrus <john.alves01@gmail.com>
2015-11-01 19:52:08 -0600

    Update

--- osproj2c.cpp
+++ osproj2c.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
-#include <string.h>
+#include <vector>
+
 
 using namespace std;
+
+vector<string>phrase;
 
 void *con(void*)
 {
 
-	/*for (int it = 0; it < phrase.size(); ++it)
+	for (int it = 0; it < phrase.size(); ++it)
 	{
 		if (phrase.front().at(it) != 'A' || phrase.front().at(it) != 'a' || phrase.front().at(it) != 'E' || phrase.front().at(it) != 'e' || phrase.front().at(it) != 'I' || phrase.front().at(it) != 'i' || phrase.front().at(it) != 'O' || phrase.front().at(it) != 'o' || phrase.front().at(it) != 'U' || phrase.front().at(it) != 'u')
 		{
@@ -20,16 +23,16 @@ void *con(void*)
 		}
 	}
 
-	pthread_exit(NULL);*/
+	pthread_exit(NULL);
 }
 
-void vow(void *phrase)
+void *vow(void*)
 {
 	sched_yield();
 
-	for (int i = 0; phrase[i] != NULL; i++)
+	for (int i = 0; i < phrase.size(); i++)
 	{
-		if (!strcmp(&phrase[i], 'A') // || phrase.front().at(i) == 'a' || phrase.front().at(i) == 'E' || phrase.front().at(i) == 'e' || phrase.front().at(i) == 'I' || phrase.front().at(i) == 'i' || phrase.front().at(i) == 'O' || phrase.front().at(i) == 'o' || phrase.front().at(i) == 'U' || phrase.front().at(i) == 'u')
+		if (phrase.front().at(i) == 'A' || phrase.front().at(i) == 'a' || phrase.front().at(i) == 'E' || phrase.front().at(i) == 'e' || phrase.front().at(i) == 'I' || phrase.front().at(i) == 'i' || phrase.front().at(i) == 'O' || phrase.front().at(i) == 'o' || phrase.front().at(i) == 'U' || phrase.front().at(i) == 'u')
 		{
 			cout << "Vow: " << phrase[i] << endl;
 		}
@@ -43,15 +46,18 @@ void vow(void *phrase)
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
 	pthread_t tid1, tid2;
-	string phrase;
+	
+	vector<string>input(argv, argv + argc);
+	phrase = input;
 
-	cout << "Please enter a phrase: ";
-	getline(cin, phrase);
+	phrase.erase(phrase.begin());
 
-	pthread_create(&tid1, NULL, &con, (void *) phrase);
+	int size = phrase.size();
+
+	pthread_create(&tid1, NULL, &con, NULL);
 	pthread_create(&tid2, NULL, &vow, NULL);
 
 	pthread_join(tid1, NULL);
